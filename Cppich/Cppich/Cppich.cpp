2d0bac6a31c797edbf727e230ab79b7b90f6c320
unknown <tsfkafsu@yandex.ru>
2025-03-07 16:33:22 +0500

    cin

--- Cppich/Cppich/Cppich.cpp
+++ Cppich/Cppich/Cppich.cpp
@@ -3,5 +3,18 @@
 
 int main()
 {
-    print("Hellow blood\n",Red);
+    consoleColor colors[] = {White,Red,Blue,Black,Green,Cyan,Yellow};
+    std::string a = "hello world";
+    for (int i = 0; i < std::size(a); i++)
+    {
+        setColor(Red);
+        print(a[i]);
+    }
+    std::cout << "\n";
+
+    std::string d = cin(Blue);
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << d;
 }
--- Cppich/Cppich/Cppich.h
+++ Cppich/Cppich/Cppich.h
@@ -1,30 +1,10 @@
 #pragma once
 #include <iostream>
 #include <string>
+
 #ifdef _WIN32
 #include <windows.h>
-
-enum consoleColor 
-{
-    Black,
-    Blue,
-    Green,
-    Cyan,
-    Red,
-    Magenta,
-    Yellow,
-    White
-};
-#else
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
+#endif
 enum consoleColor
 {
     Black,
@@ -36,48 +16,44 @@ enum consoleColor
     Yellow,
     White
 };
-#endif
 
 #ifdef _WIN32
-template <typename T>
-void print(T text,consoleColor col = White)
-{
+void setColor(consoleColor col) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, col);
-    std::cout << text;
 }
 #else
-template <typename T>
-void print(T text, consoleColor col = White)
-{
-    switch (col)
-    {
-    case Black:
-        std::cout << BLACK << text;
-        break;
-    case Blue:
-        std::cout << BLUE << text;
-        break;
-    case Green:
-        std::cout << GREEN << text;
-        break;
-    case Cyan:
-        std::cout << CYAN << text;
-        break;
-    case Red:
-        std::cout << RED << text;
-        break;
-    case Magenta:
-        std::cout << MAGENTA << text;
-        break;
-    case Yellow:
-        std::cout << YELLOW << text;
-        break;
-    case White:
-        std::cout << WHITE << text;
-        break;
-    default:
-        break;
+void setColor(consoleColor col) {
+    switch (col) {
+    case Black:   std::cout << "\033[30m"; break;
+    case Blue:    std::cout << "\033[34m"; break;
+    case Green:   std::cout << "\033[32m"; break;
+    case Cyan:    std::cout << "\033[36m"; break;
+    case Red:     std::cout << "\033[31m"; break;
+    case Magenta: std::cout << "\033[35m"; break;
+    case Yellow:  std::cout << "\033[33m"; break;
+    case White:   std::cout << "\033[37m"; break;
     }
 }
 #endif
+template <typename T>
+void print(T text, consoleColor col = White) {
+    setColor(col);
+    std::cout << text;
+#ifdef _WIN32
+    setColor(White);
+#else
+    std::cout << "\033[0m";
+#endif
+}
+std::string cin(consoleColor col = White) {
+    setColor(col);
+    std::string text;
+    std::getline(std::cin, text);
+#ifdef _WIN32
+    setColor(White);
+#else
+    std::cout << "\033[0m";
+#endif
+    return text;
+}
